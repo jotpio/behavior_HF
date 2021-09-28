e8773c75ae8fe961153e5c659da3e1d34f2d7dc4
Aerius01 <david.james.dj551@gmail.com>
2021-09-28 08:01:22 +0200

    add pos, dir to robot log every 5 ticks

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ formatter = logging.Formatter("%(asctime)s %(levelname) -8s %(message)s")
 
 logger = logging.getLogger("user_input_logger")
 handler = TimedRotatingFileHandler(
-    "/home/user1/RoboTracker_HF/logs/user_input/user_input", when="S", interval=5
+    "/home/hf-robofish/Desktop/user_input", when="S", interval=5
 )
 handler.setFormatter(formatter)
 # handler.setLevel(logging.CRITICAL)
@@ -20,14 +20,14 @@ for _ in range(10):
 
 print("test1234434234234")
 
-logger2 = logging.getLogger("debug_logger")
+# logger2 = logging.getLogger("debug_logger")
 
-handler2 = TimedRotatingFileHandler(
-    "/home/user1/RoboTracker_HF/logs/behavior_prints/out", when="S", interval=5
-)
-logger2.addHandler(handler2)
-logger2.setLevel(logging.DEBUG)
+# handler2 = TimedRotatingFileHandler(
+#     "/home/user1/RoboTracker_HF/logs/behavior_prints/out", when="S", interval=5
+# )
+# logger2.addHandler(handler2)
+# logger2.setLevel(logging.DEBUG)
 
-for _ in range(10):
-    logger2.info(_)
-    time.sleep(1)
+# for _ in range(10):
+#     logger2.info(_)
+#     time.sleep(1)
